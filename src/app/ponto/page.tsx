c3e53ca43e82295d6488d182adb05954ff393ccb
Karielly <kariellyycarvalho@gmail.com>
2025-08-26 15:42:58 -0300

    Atualiza importação do componente LoadingCard para o novo caminho em 'src/components/ponto' e remove o arquivo 'LoadingCard.tsx' da pasta 'funcionario'.

--- src/app/ponto/page.tsx
+++ src/app/ponto/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { LoadingCard } from "@/components/funcionario/LoadingCard";
+import { LoadingCard } from "@/components/ponto/LoadingCard";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
